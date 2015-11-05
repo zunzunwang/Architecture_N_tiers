973124a6d66d4335b87e519b590527af8e28c29c
yishuo <yishuo.lyu@gmail.com>
2015-11-06 00:17:06 +0100

    The windows of choosing
    The element that the choice will be magnified if we choose this choice
    has been completed

--- js/choice.js
+++ js/choice.js
@@ -1,7 +1,19 @@
 var direction = angular.module('choice',[]);
 direction.controller('machoice',['$scope',function($scope,Data){
+
 	$scope.bdelogoSmall = true;
+	$scope.tselogoSmall = true;
 	$scope.bdelogoLarge = false;
-	$scope.tselogoSmall = false;
-	$scope.tselogoLarge = true;
+	$scope.tselogoLarge = false;
+
+	$scope.showEtHideBDE = function(){  
+		$scope.bdelogoSmall = !$scope.bdelogoSmall;
+		$scope.bdelogoLarge = !$scope.bdelogoLarge;
+	};
+
+	$scope.showEtHideTSE = function(){
+		$scope.tselogoSmall = !$scope.tselogoSmall;
+		$scope.tselogoLarge = !$scope.tselogoLarge;
+	};
+
 }]);
