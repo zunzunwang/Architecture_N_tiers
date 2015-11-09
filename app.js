50798bea0cb2bfe513caa94962205134cc410601
yishuo <yishuo.lyu@gmail.com>
2015-11-09 19:15:47 +0100

    The page of upload.html
    The page of upload.html for publishing your produit, this is a small
    problem of stylesheet but its fonction is well used.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var boutique = angular.module('Boutique', [
-	'ui.router','ui.bootstrap'
+	'ui.router','ui.bootstrap','ui.uploader'
 ]);
 
 boutique.config(function($stateProvider, $urlRouterProvider){
@@ -67,39 +67,39 @@ boutique.controller('showHide', ['$scope', function ($scope,Data) {
 	$scope.returnToday();
 
 
-    $scope.showEtHide = function(){  
-      // $scope.slidesLooping = !$scope.slidesLooping;
-      $scope.hideImg = !$scope.hideImg;
-      $scope.showImg = !$scope.showImg;
-      $scope.slidesLooping = !$scope.slidesLooping;
-    }
+  $scope.showEtHide = function(){  
+    // $scope.slidesLooping = !$scope.slidesLooping;
+    $scope.hideImg = !$scope.hideImg;
+    $scope.showImg = !$scope.showImg;
+    $scope.slidesLooping = !$scope.slidesLooping;
+  }
 
-    // var timesDoubleClick = 0;
-    $scope.important = function() {
-        var dayImpo = $scope.dt;
-        $scope.events =
-            [
-              {
-                date: dayImpo,
-                status: 'important'
-              }
-            ];
+  // var timesDoubleClick = 0;
+  $scope.important = function() {
+    var dayImpo = $scope.dt;
+    $scope.events =
+      [
+        {
+          date: dayImpo,
+          status: 'important'
+        }
+      ];
 
-        $scope.getDayClass = function(date, mode) {
-          if (mode === 'day') {
-            var dayToCheck = new Date(date).setHours(0,0,0,0);
-            for (var i=0;i<$scope.events.length;i++){
-              var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+    $scope.getDayClass = function(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0,0,0,0);
+        for (var i=0;i<$scope.events.length;i++){
+          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
 
-              if (dayToCheck === currentDay) {
-                return $scope.events[i].status;
-              }
-            }
+          if (dayToCheck === currentDay) {
+            return $scope.events[i].status;
           }
-          return '';
-        };
+        }
+      }
+      return '';
+    };
 
-    }
+  }
 
 }]);
 
@@ -123,3 +123,36 @@ boutique.controller('imageChanged', ['$scope', function ($scope) {
   	});
 
 }]);
+
+/*//controller of upload.html
+boutique.controller('uploaderCtrl', ['$scope', function($scope, $log, uiUploader) {
+  $scope.btn_remove = function(file) {
+    $log.info('deleting=' + file);
+    uiUploader.removeFile(file);
+  };
+  $scope.btn_clean = function() {
+    uiUploader.removeAll();
+  };
+  $scope.btn_upload = function() {
+    $log.info('uploading...');
+    uiUploader.startUpload({
+      url: 'http://realtica.org/ng-uploader/demo.html',
+      concurrency: 2,
+      onProgress: function(file) {
+        $log.info(file.name + '=' + file.humanSize);
+        $scope.$apply();
+      },
+      onCompleted: function(file, response) {
+        $log.info(file + 'response' + response);
+      }
+    });
+  };
+  $scope.files = [];
+  var element = document.getElementById('file1');
+  element.addEventListener('change', function(e) {
+    var files = e.target.files;
+    uiUploader.addFiles(files);
+    $scope.files = uiUploader.getFiles();
+    $scope.$apply();
+  });
+}]);*/
